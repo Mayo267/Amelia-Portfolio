6b4ddc2c7a7d866c00e8bcfe527e7a76d904f561
Andrew Mayo <andrewmayo@Andrews-MacBook-Pro.local>
2019-01-19 18:18:45 -0500

    more changes

--- Assets/app.js
+++ Assets/app.js
@@ -1,1 +1,9 @@
+$( document ).ready(function() {
+    // About button scrolling
+    $("#bio-button").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#about-box").offset().top
+        }, 1000);
+    });
 
+});
